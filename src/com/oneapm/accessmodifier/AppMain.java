72ef3675fb966eb0851669531060e3423fcb08e6
liupanpan <liupanpan@oneapm.com>
2016-03-22 22:40:17 +0800

    1.类访问修饰符测试 2.方法或属性的访问修饰符测试

--- src/com/oneapm/accessmodifier/AppMain.java
+++ src/com/oneapm/accessmodifier/AppMain.java
@@ -2,6 +2,45 @@ package com.oneapm.accessmodifier;
 
 /**
  * Created by Administrator on 2016/3/22.
+ * Purpose:
+ * 类修饰符:        public    -> 在所有的地方都能访问
+ *                  默认      -> 在同一包内被访问
+ *方法/属性修饰符 ：public    -> 在所有的地方都能访问(前提是类能访问)
+ *                  protected -> 在同一包内及不同包的子类中
+ *                  default   -> 在同一包内
+ *                  private   -> 在同一类中
  */
+
+ class MyClass{
+    private int privateVar;
+    int defaultVar ;
+    protected int protectedVar ;
+    public int publicVar ;
+
+    private void privateMethod(){
+    }
+
+    protected void protectedMethod(){
+    }
+
+    void defaultMethod(){
+    }
+
+    public void publicMethod(){
+    }
+}
+
 public class AppMain {
+    public static void main(String[] args) {
+        MyClass instance = new MyClass() ;
+        int var = instance.defaultVar ;
+        var = instance.protectedVar ;
+        var  = instance.publicVar ;
+
+        instance.defaultMethod();
+        instance.protectedMethod();
+        instance.publicMethod();
+
+
+    }
 }
